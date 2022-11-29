86a45a536715ff2a42ebac5a20d29359cd9e52ec
RiftStudier <98359096+AidanPohl@users.noreply.github.com>
2022-11-28 22:37:12 -0600

    wipping

--- UnityAgonDray/Assets/AnimationTriggerCollision.cs
+++ UnityAgonDray/Assets/AnimationTriggerCollision.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(Animator))]
 public class AnimationTriggerCollision : MonoBehaviour
 {
+    public Animator animator;
+    public string targetTag;
+    public string animParameter;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(targetTag))
+        {
+            animator.SetBool(animParameter, true);
+        }
     }
 }
